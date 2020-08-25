990d89af0e21f1a968d88ec7b0fb625c5912ceaa
Mateusz Migdał <mateusz.migdal@axbit.com>
2020-08-25 16:03:34 +0200

    chore: module 5 completed

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store } from '@ngrx/store';
+import { State, getShowProductCode } from '../state/product.reducer';
 
 @Component({
   selector: 'pm-product-list',
@@ -25,7 +26,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   constructor(
     private productService: ProductService,
-    private store: Store<any>
+    private store: Store<State>
   ) {}
 
   ngOnInit(): void {
@@ -38,11 +39,9 @@ export class ProductListComponent implements OnInit, OnDestroy {
       error: (err) => (this.errorMessage = err),
     });
 
-    this.store.select('products').subscribe((products) => {
-      if (products) {
-        this.displayCode = products.showProductCode;
-      }
-    });
+    this.store
+      .select(getShowProductCode)
+      .subscribe((showProductCode) => (this.displayCode = showProductCode));
   }
 
   ngOnDestroy(): void {
--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,11 +1,55 @@
-import { createReducer, createAction } from '@ngrx/store';
+import {
+  createReducer,
+  on,
+  createAction,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
+import { Product } from '../product';
+import * as AppState from '../../state/app.state';
 
-import { on } from '@ngrx/store';
+export interface ProductState {
+  showProductCode: boolean;
+  currentProductId: number;
+  products: Product[];
+}
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] toggle Product Code'), (state) => {
-    console.log('original state: ' + JSON.stringify(state));
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProductId: null,
+  products: [],
+};
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  (state) => state.showProductCode
+);
+
+export const getCurrentProductId = createSelector(
+  getProductFeatureState,
+  (state) => state.currentProductId
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  getCurrentProductId,
+  (state, id) => state.products.find((p) => p.id === id)
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  (state) => state.products
+);
+
+export const productReducer = createReducer<ProductState>(
+  initialState,
+  on(createAction('[Product] toggle Product Code'), (state: ProductState) => {
     return {
       ...state,
       showProductCode: !state.showProductCode,
