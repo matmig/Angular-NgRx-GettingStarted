46808240707a609fbd103d24d7a4e3cda6b72f20
Mateusz Migdał <mateusz.migdal@axbit.com>
2020-08-25 14:36:51 +0200

    chore: homework app-demo1

--- APM-Demo0/src/app/user/user.module.ts
+++ APM-Demo0/src/app/user/user.module.ts
@@ -4,18 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 import { LoginComponent } from './login.component';
+import { StoreModule } from '@ngrx/store';
+import { userReducer } from './store/userReducer';
 
-const userRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
-];
+const userRoutes: Routes = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild(userRoutes)
+    RouterModule.forChild(userRoutes),
+    StoreModule.forFeature('user', userReducer),
   ],
-  declarations: [
-    LoginComponent
-  ]
+  declarations: [LoginComponent],
 })
-export class UserModule { }
+export class UserModule {}
